0276fc07a7a2b0a4595e7c4f58d5a244deccd6a8
Shone <shonegr28@gmail.com>
2024-12-26 19:45:50 -0600

    created env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+require ("dotenv").config();
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 // connection string with db-password db-name, password is leaked here, also manually have to put db-name here after .net/
-mongoose.connect("mongodb+srv://pravachanpatra:5ct6fwHnaGaUJsDA@coding1v1platformcluste.kvwqv.mongodb.net/coding1v1platform?retryWrites=true&w=majority&appName=coding1V1platformcluster0")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log("Error connecting to MongoDB:", err));
 

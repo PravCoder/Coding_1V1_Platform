c42ff1d763224d4ed48412e0de89805fec874a5a
Shone Renjan <shonerenjan@Mac-mini.attlocal.net>
2025-04-13 12:56:16 -0500

    tailwind added

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,16 @@
+export const navLinks = [
+    { href: "/", label: "Home" },
+    { href: "/aboutpage", label: "About Us" },
+    { href: "/login", label: "Login" },
+    
+];
+
+export const headerTitle =[
+    {subTitle: "TEST YOUR LIMITS"},
+];
+
+export const aboutdescription =[
+    {subTitle: "About Us"},
+    {description: "We are a team of passionate developers and designers who love to create amazing web applications. Our goal is to provide the best user experience possible. how to use our platform to test your coding skills and improve your problem-solving abilities. We offer a variety of coding challenges and competitions that will help you sharpen your skills and prepare for technical interviews. Join us today and start your coding journey!"},
+];
+

bd4c25622814143b14971c58f72d22195b8aa928
Pravachan Patra <pravachanpatra@gmail.com>
2025-04-14 13:28:51 -0500

    username unique

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    username: {type:String, required:true, unique:true},
-    email: {type:String, required:true, unique:true},
+    username: {type:String, required:true, unique:false},  // not unique
+    
+    email: {type:String, required:true, unique:true},  // unique
     password: {type:String, required:true},
 
     matches: [{type: mongoose.Schema.Types.ObjectId, ref: "Match" }]
